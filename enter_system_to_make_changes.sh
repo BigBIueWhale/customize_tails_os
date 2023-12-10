edce838819234d251a7ba2272546b4d2e377eea6
Ronen Zyroff <rzyroff@gmail.com>
2023-12-10 07:59:07 -0500

    Removing GUI seems to not be that hard

--- enter_system_to_make_changes.sh
+++ enter_system_to_make_changes.sh
@@ -2,7 +2,8 @@
 
 sudo chroot ./build/tails-squashfs/
 # Now you're in a terminal in which you can modify that tails OS.
-# For example:
-# sudo apt-get purge <package-name>
+# For example, to disable GUI run:
+# sudo apt-get purge xorg*
+# sudo apt-get purge gnome*
 # sudo apt-get autoremove
 # sudo apt-get clean
