0eaa565fe4daf6e41cab79e683f3e481e7c66c59
Ronen Zyroff <rzyroff@gmail.com>
2023-12-20 11:51:07 -0500

    Made run_at_boot.sh executable

--- files_to_include_in_os/program1/main.cpp
+++ files_to_include_in_os/program1/main.cpp
@@ -17,7 +17,7 @@ int main() {
     std::signal(SIGINT, signalHandler);
     std::signal(SIGTERM, signalHandler);
 
-    std::cout << "Program 1 running on boot";
+    std::cout << "Program 1 running on boot" << std::endl;
 
     int counter = 0;
 
