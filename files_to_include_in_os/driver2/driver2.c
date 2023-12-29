9af1cd6e5ed80775436da5cf936ea25abdca87c1
Ronen Zyroff <rzyroff@gmail.com>
2023-12-29 16:39:01 -0500

    Add source code for 2 example drivers

---
+++ files_to_include_in_os/driver2/driver2.c
@@ -0,0 +1,17 @@
+#include <linux/module.h>       // Needed for all modules
+#include <linux/kernel.h>       // Needed for KERN_INFO
+
+int init_module(void)
+{
+    printk(KERN_INFO "driver2: Hello to kernel log\n");
+
+    // A non 0 return means init_module failed; module can't be loaded.
+    return 0;
+}
+
+void cleanup_module(void)
+{
+    printk(KERN_INFO "driver2: Goodbye to kernel log\n");
+}
+
+MODULE_LICENSE("GPL");
